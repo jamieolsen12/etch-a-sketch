f2f46ab7ee7c785c55dcf440bb6b038d7a1c601c
Jamie Olsen <jamieolsen12@gmail.com>
2023-03-21 15:27:08 +0700

    create addHoverColor function taking div as input, add event listener to div to change background color when hovered over. Declare target color variable to be changed later

--- app.js
+++ app.js
@@ -8,26 +8,45 @@
 //  1e. repeat process 16x
 //  1f. Have 256 boxes with same class and unique IDs
 
+// Color each cell will change its background color property to when clicked
+let targetColor = "green";
 
 
 let boxGrid = document.querySelector('.box-grid');
 
 let idCounter = 1
 
-
+// create 16 rows
 for (i = 0; i < 16; i++) {
     var row = document.createElement("div");
     row.classList.add("row");
 
+    // create 16 cells within each row with cell class and unique ID
     for (j = 0; j < 16; j++) {
         var cell = document.createElement('div');
         cell.classList.add('cell');
+        
+        // show ID in box to aid setup for now, will remove later
         cell.id = String(idCounter);
         cell.textContent = idCounter;
         idCounter++;
+        addHoverColor(cell);
+
+        // add each cell to parent container
         row.appendChild(cell);
     }
+
+    // add each row to parent container
     boxGrid.appendChild(row);
 }
+
+
+// function to change the div (cell) background colour when hovered over
+function addHoverColor(div) {
+    div.addEventListener("mouseover", function() {
+        div.style.backgroundColor = targetColor;
+    })
+}
+
 
 
