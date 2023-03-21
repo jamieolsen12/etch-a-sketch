199194efe6eec158224c462979c43189d94875ce
Jamie Olsen <jamieolsen12@gmail.com>
2023-03-21 21:17:32 +0700

    refactor some code. add onchange method to reload grid with slider value, and onmousemove method to show current ratio in the box

--- app.js
+++ app.js
@@ -11,24 +11,81 @@
 // Color each cell will change its background color property to when clicked
 let drawColor = "green";
 let bgColor = "white";
-
-let boxLength = 25;
-
-let grid = document.querySelector('.grid');
+let currentSize = 16;
 
 let idCounter = 1
+let allCells = [];
 
+// functions to change basic elements
+function setDrawColorTo(newColor) {
+    drawColor = newColor;
+}
+
+function setBgColorTo(newColor) {
+    bgColor = newColor;
+    applyBgColor();
+}
+
+function setCurrentSizeTo(newSize) {
+    currentSize = newSize;
+}
+
+
+
+// Objects
 const drawColorPicker = document.getElementById('drawColorPicker');
 const bgColorPicker = document.getElementById('bgColorPicker');
+const sizeSlider = document.getElementById('sizeSlider');
+let grid = document.querySelector('.grid');
 
 // create the grid
-createGrid(boxLength);
+createGrid(currentSize);
 
-// array of all cells
-let allCells = document.querySelectorAll('.cell');
+// create array of all cells, call at the end of createGrid()
+function updateCellList() {
+    allCells = document.querySelectorAll('.cell');
+    console.log(allCells);
+}
+
+// call setDrawColorTo() when whenever new color is selected from colorPicker
+drawColorPicker.oninput = (e) => setDrawColorTo(e.target.value);
+
+// call setBgColorTo() whenever new color is selected from bgColorPicker
+bgColorPicker.oninput = (e) => setBgColorTo(e.target.value);
+
+// use sizeSlider to adjust the size of the grid
+sizeSlider.onchange= (e) => {changeSize(e.target.value)
+}
+
+// display current size on slider immediately
+sizeSlider.onmousemove = (e) => updateSizeValue(e.target.value)
+
+function changeSize(newSize) {
+    setCurrentSizeTo(newSize);
+    updateSizeValue(newSize);
+    reloadGrid();
+}
+
+// function to change text to show current grid size
+function updateSizeValue(value) {
+    sizeValue.innerHTML = `${value} x ${value}`
+  }
+
+function reloadGrid() {
+    clearGrid();
+    createGrid(currentSize);
+}
+  
+function clearGrid() {
+    grid.innerHTML = '';
+}
 
 
-// function to set the background color of all cells
+
+
+
+
+// function to apply the background color to all cells
 function applyBgColor() {
     allCells.forEach((cell) => {
         if (cell.classList.contains("inked")) {
@@ -39,52 +96,36 @@ function applyBgColor() {
     })
 }
 
-// function to change draw color
-function setDrawColorTo(newColor) {
-    drawColor = newColor;
-}
-
-// function to change background color
-function setBgColorTo(newColor) {
-    bgColor = newColor;
-    applyBgColor();
-}
-
-// call setDrawColorTo() when whenever new color is selected from colorPicker
-drawColorPicker.oninput = (e) => setDrawColorTo(e.target.value);
-
-// call setBgColorTo() whenever new color is selected from bgColorPicker
-bgColorPicker.oninput = (e) => {
-    setBgColorTo(e.target.value);
-}
 
 
 
-function createGrid(boxLength){
-// create boxLength rows
-for (i = 0; i < (boxLength); i++) {
-    var row = document.createElement("div");
-    row.classList.add("row");
 
-    // create boxLength cells within each row with cell class and unique ID
-    for (j = 0; j < (boxLength); j++) {
-        var cell = document.createElement('div');
-        cell.classList.add('cell');
+
+function createGrid(currentSize) {
+    for (i = 0; i < (currentSize); i++) {
+        var row = document.createElement("div");
+        row.classList.add("row");
+
+        // create currentSize cells within each row with cell class and unique ID
+        for (j = 0; j < (currentSize); j++) {
+            var cell = document.createElement('div');
+            cell.classList.add('cell');
+            
+            // show ID in box to aid setup for now, will remove later
+            cell.id = String(idCounter);
+            // cell.textContent = String(idCounter);
+            idCounter++;
+            addHoverColor(cell);
         
-        // show ID in box to aid setup for now, will remove later
-        cell.id = String(idCounter);
-        // cell.textContent = String(idCounter);
-        idCounter++;
-        addHoverColor(cell);
-       
 
-        // add each cell to parent container
-        row.appendChild(cell);
+            // add each cell to parent container
+            row.appendChild(cell);
+        }
+
+        // add each row to parent container
+        grid.appendChild(row);
     }
-
-    // add each row to parent container
-    grid.appendChild(row);
-}
+    updateCellList() 
 }
 
 // function to change the div (cell) background colour when hovered over
